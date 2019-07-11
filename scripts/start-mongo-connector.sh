f21f577b1fbdea3449df38c1e4c164464c29c2aa
vurankar <vurankar@riffyn.com>
2019-07-11 17:47:39 -0400

    SEAR-82, Parameteize target host for elastic doc manager Removed the hardcoded elasticsearch server address from connector configs and provide ES host details from environment variables in the command line

--- scripts/start-mongo-connector.sh
+++ scripts/start-mongo-connector.sh
@@ -4,6 +4,7 @@ bash elasticsearch-configure.sh -f
 
 # expect INDEX_LIST environment variable to provide the list of ES indexes this instance of mongo-connector
 # should work for. Expect a single index name or a comma separated list
+# Eg: "resource_types"  or " resource_types,property_types,resources_and_run_data"
 INDEX_LIST=${INDEX_LIST:-""}
 
 echo "setting mongo-connector for indices ${INDEX_LIST}"
@@ -16,6 +17,6 @@ for index in "${INDEX_ARRAY[@]}"
 do
     # $MONGO_HOSTS should be in format HOSTNAME:PORT
     # example mongodb01:27017,mongodb02:27017,mongodb03:27017
-    mongo-connector --auto-commit-interval=0 -m $MONGO_HOSTS -c config/connector_$index.json --stdout
+    mongo-connector --auto-commit-interval=0 -m $MONGO_HOSTS -c config/connector_$index.json -t $ELASTIC_HOST:$ELASTIC_PORT --stdout
     sleep 1
 done
