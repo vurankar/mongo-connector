f12f58890362f31db0fe528c4737351d302a65b3
Lili Nader <lnader@riffyn.com>
2020-02-26 18:06:05 -0800

    OPS-335 Enable SSL connections for mongodb

--- scripts/start-mongo-connector.sh
+++ scripts/start-mongo-connector.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-OPLOG_TIMESTAMP_LOCATION="/srv/riffyn/mongo-connector/oplogts"
-declare -A INDEX_NAME_MAP=( ["resourcetypes"]="resource_types"
-                     ["propertytypes"]="property_types"
-                     ["resourcesandrundata"]="resources_and_run_data"
-
-                    )
-
+OPLOG_TIMESTAMP_LOCATION="/srv/riffyn/mongo-connector/oplogts/oplog.timestamp"
 
 RESET_INDEX=${RESET_INDEX:-0}
 #SKIP_INDEX_RESET  : 0: do not reset and 1: reset index
@@ -15,35 +9,59 @@ RESET_INDEX=${RESET_INDEX:-0}
 echo " value of RESET_INDEX: ${RESET_INDEX}"
 echo " value of ELASTIC_HOST: ${ELASTIC_HOST}"
 echo " value of ELASTIC_PORT: ${ELASTIC_PORT}"
+echo " value of ELASTIC_USER: ${ELASTIC_USER}"
+echo " value of ELASTIC_PASSWORD: ${ELASTIC_PASSWORD}"
 echo " value of MONGO_HOSTS: ${MONGO_HOSTS}"
 echo " value of INDEX_NAME: ${INDEX_NAME}"
 
+echo "setting TARGET_URL for elasticsearch"
+TARGET_URL="${ELASTIC_HOST}:${ELASTIC_PORT}"
+
+if [[ -z "${ELASTIC_USERNAME}" &&  -z "${ELASTIC_PASSWORD}" ]]; then
+  echo "Elasticsearch username and password undefined proceeding without authentication"
+else
+  echo "Elasticsearch username and password defined adding to TARGET_URL"
+  TARGET_URL="${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}@${ELASTIC_HOST}:${ELASTIC_PORT}"
+fi
+
 # check if the index exists. If the index is absent in ES, continue with RESET INDEX flow
 # is there a easier way to check if a index is present in ElasticSearch ?
-if [ $(curl --write-out %{http_code} --silent --output /dev/null "$ELASTIC_HOST:$ELASTIC_PORT/$INDEX_NAME") == 200 ];
+INDEX_CHECK_CURL_COMMAND="curl -k --write-out %{response_code} --silent --output /dev/null https://${TARGET_URL}/${INDEX_NAME}"
+INDEX_CHECK=$($INDEX_CHECK_CURL_COMMAND)
+echo "Response code from curl request to check ${INDEX_NAME} = ${INDEX_CHECK}"
+
+if [ $INDEX_CHECK == 200 ];
 then
+  echo "Index ${INDEX_NAME} already exists"
+else
   RESET_INDEX="1"
-  echo "${INDEX_NAME} not found on ElasticSearch. Continue with RESET INDEX flow"
+  echo "Index ${INDEX_NAME} not found on ElasticSearch. Continue with RESET INDEX flow"
 fi
 
 if [ "$RESET_INDEX" == "1" ]
 then
+    echo "Resetting elasticsearch index ${INDEX_NAME}...."
     bash elasticsearch-configure.sh -f
+    echo "Checking to see ${INDEX_NAME} was created...."
+    INDEX_CHECK=$($INDEX_CHECK_CURL_COMMAND)
+    echo "Response code from curl request to check ${INDEX_NAME} = ${INDEX_CHECK}"
 else
     echo "skipping elastic search configure because RESET_INDEX is set to $RESET_INDEX"
 fi
 
-INDEX_NAME=${INDEX_NAME:-""}
-# convert input K8 friendly index name to actual index name
-# Eg: resourcetypes -> resource_types
-INDEX_NAME=${INDEX_NAME_MAP[$INDEX_NAME]}
+MONGO_SSL_ARGS=""
 
+if [ "$MONGO_SSL_ENABLED" == "true" ]
+then
+    echo "SSL enabled for MongoDB adding SSL args"
+    MONGO_SSL_ARGS="--ssl-certfile /etc/ssl/mongodb/client.pem --ssl-certificate-policy ignored"
+fi
 
 echo "setting mongo-connector for index ${INDEX_NAME}"
 
 # $MONGO_HOSTS should be in format HOSTNAME:PORT
 # example mongodb01:27017,mongodb02:27017,mongodb03:27017
-mongo-connector --auto-commit-interval=0 -m $MONGO_HOSTS -c config/connector_${INDEX_NAME}.json --oplog-ts ${OPLOG_TIMESTAMP_LOCATION}/oplog.timestamp  -t $ELASTIC_HOST:$ELASTIC_PORT --stdout
+mongo-connector --auto-commit-interval=0 $MONGO_SSL_ARGS -m $MONGO_HOSTS -c "config/connector_${INDEX_NAME}.json" --oplog-ts ${OPLOG_TIMESTAMP_LOCATION}  -t $TARGET_URL --stdout
 #mongo-connector --auto-commit-interval=0 -m $MONGO_HOSTS -c config/connector_${INDEX_NAME}.json --oplog-ts ${OPLOG_TIMESTAMP_LOCATION}/oplog.timestamp -t $ELASTIC_USER:$ELASTIC_PASSWORD@$ELASTIC_HOST:$ELASTIC_PORT --stdout
 
 sleep 10
